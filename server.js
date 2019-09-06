adfc957f2315de84cbefcd1df7186fec50e05875
Brandt Strom <brandtkstrom@gmail.com>
2019-09-05 21:20:56 -0700

    set view engine to handlebars

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+var exphbs = require('express-handlebars');
 const router = require('./controllers/burgers_controller');
 
 const burgerApp = express();
 const PORT = process.env.PORT || 8080;
 
+burgerApp.engine('handlebars', exphbs());
+burgerApp.set('view engine', 'handlebars');
 burgerApp.use('/', router);
 
 burgerApp.listen(PORT, () => `Server running on port: ${PORT}`);
