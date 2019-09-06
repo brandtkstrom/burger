949fe85ec4865c3ee00544e007122b02dd6a2cf4
Brandt Strom <brandtkstrom@gmail.com>
2019-09-05 22:42:49 -0700

    implement orm selectAll

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,18 @@
 const DB = require('./connection');
 
+const checkForError = err => {
+    if (err) {
+        throw err;
+    }
+};
+
 const ORM = {
     selectAll: () => {
         // TODO - DB select all
+        DB.query('SELECT * FROM burgers', (err, data) => {
+            checkForError(err);
+            return data;
+        });
     },
     insertOne: () => {
         // TODO - DB insert one
