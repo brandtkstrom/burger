0d4cceb04e7178897c4d22a69c592c6c3e1f8b7c
Brandt Strom <brandtkstrom@gmail.com>
2019-09-06 02:32:04 -0700

    create burger.js for client js logic

---
+++ public/scripts/burger.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#submit-burger').addEventListener('click', evt => {
+        evt.preventDefault();
+
+        console.log('submit button pressed!!');
+        // Get burger input
+
+        // Add burger to DB
+
+        // Render view
+    });
+});
