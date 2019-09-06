b9f9e3037dd5acc00402a308da4aacaefee0dc21
Brandt Strom <brandtkstrom@gmail.com>
2019-09-06 00:21:51 -0700

    fix express listen log callback

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
-var exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const router = require('./controllers/burgers_controller');
 
 const burgerApp = express();
 const PORT = process.env.PORT || 8080;
 
+burgerApp.use(express.static('public'))
 burgerApp.engine('handlebars', exphbs());
 burgerApp.set('view engine', 'handlebars');
 burgerApp.use('/', router);
 
-burgerApp.listen(PORT, () => `Server running on port: ${PORT}`);
+burgerApp.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
