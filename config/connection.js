de80ac49024d7033f7476d65261d2c0f5da833da
Brandt Strom <brandtkstrom@gmail.com>
2019-09-06 00:22:08 -0700

    update connection to optionally use JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,20 @@
 const mysql = require('mysql');
 
-const DB = mysql.createConnection({
+const localConfig = {
     host: '127.0.0.1',
     port: 3306,
     user: 'root',
-    password: 'turbo123',
+    password: 'Password1',
     database: 'burgers_db'
+};
+
+const dbConfig = mysql.env.JAWSDB_URL || localConfig;
+
+const DB = mysql.createConnection(dbConfig);
+
+DB.connect(err => {
+    if (err) throw err;
+    console.log("Successfully connected to database.");
 });
 
 module.exports = DB;
