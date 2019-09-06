1508fb393b3514b45266f9ee1452d8b2bf3e0565
Brandt Strom <brandtkstrom@gmail.com>
2019-09-06 05:07:10 -0700

    implement client side requests

--- public/scripts/burger.js
+++ public/scripts/burger.js
@@ -2,11 +2,49 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#submit-burger').addEventListener('click', evt => {
         evt.preventDefault();
 
-        console.log('submit button pressed!!');
         // Get burger input
+        const newBurger = document.querySelector('#input-burger').value.trim().toLowerCase();
+        if (newBurger === '') {
+            return; // No input -> return early
+        }
 
         // Add burger to DB
+        fetch(`/burgers/${newBurger}`, { method: 'POST' })
+            .then(res => {
+                if (res.status === 200) {
+                    window.location.reload();
+                }
+            })
+            .catch(err => {
+                alert('Error adding new burger: ' + err);
+            });
+    });
 
-        // Render view
+    document.querySelector('#button-reset').addEventListener('click', evt => {
+        evt.preventDefault();
+
+        fetch('/burgers', { method: 'DELETE' })
+        .then(res => {
+            if (res.status === 200) {
+                window.location.reload();
+            }
+        })
+        .catch(err => {
+            alert('Error reseting the burgers DB: ' + err);
+        });
+    });
+
+    document.addEventListener('click', e => {
+        if (e.target && e.target.className === 'devour') {
+            fetch(`/burgers/${e.target.id}`, { method: 'PUT' })
+                .then(res => {
+                    if (res.status === 200) {
+                        window.location.reload();
+                    }
+                })
+                .catch(err => {
+                    alert('Error devouring burger! - ' + err);
+                });
+        }
     });
 });
