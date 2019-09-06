3c0e322899f406b33953d84a0a198273bf0c9b83
Brandt Strom <brandtkstrom@gmail.com>
2019-09-06 05:06:21 -0700

    finish orm implementation

--- config/orm.js
+++ config/orm.js
@@ -6,25 +6,48 @@ const checkForError = err => {
     }
 };
 
+const readAllBurgers = callback => {
+    DB.query('SELECT * FROM burgers', (err, data) => {
+        checkForError(err);
+        callback(data);
+    });
+};
+
 const ORM = {
     // Select all burgers from the DB
-    selectAll: (callback) => {
-        return DB.query('SELECT * FROM burgers', callback);
-    },
+    selectAll: callback => readAllBurgers(callback),
     // Insert new burger into the DB
-    insertOne: burgerName => {
-        // TODO - DB insert one
+    insertOne: (burgerName, callback) => {
         DB.query(
-            'INSERT INTO burgers (burger_name) VALUES ?',
-            burgerName,
+            `INSERT INTO burgers (burger_name) VALUES ('${burgerName}')`,
             err => {
                 checkForError(err);
+
+                // Get updated burger collection from DB
+                readAllBurgers(callback);
             }
         );
     },
     // Update existing burger in the DB
-    updateOne: () => {
-        // TODO DB update one
+    updateOne: (id, callback) => {
+        DB.query(
+            'UPDATE burgers SET ? WHERE ?',
+            [{ devoured: true }, { id: id }],
+            err => {
+                checkForError(err);
+
+                // Get updated burger collection from DB
+                readAllBurgers(callback);
+            }
+        );
+    },
+    resetDb: callback => {
+        DB.query('DELETE FROM burgers', err => {
+            checkForError(err);
+
+            // Refresh burger collection from DB
+            readAllBurgers(callback);
+        });
     }
 };
 
