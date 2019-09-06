a18295c650d9bd3b2b0d9c0334c9d7e29667c346
Brandt Strom <brandtkstrom@gmail.com>
2019-09-06 05:06:41 -0700

    finalize burgers_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,33 +1,40 @@
 const express = require('express');
-const orm = require('../config/orm')
+const orm = require('../config/orm');
 const router = express.Router();
 
+// Callback used to organize and render burgers
+const renderBurgers = (res, data) => {
+    const uneaten = data.filter(b => !b.devoured);
+    const devoured = data.filter(b => b.devoured);
+
+    res.render('index', { uneaten, devoured });
+};
+
 router.get('/', (req, res) => {
-
-    // Callback used to render burgers
-    const renderBurgers = (err, data) => {
-        if (err) {
-            throw err;
-        }
-
-        console.log(data);
-
-        const uneaten = data.filter(b => !b.devoured);
-        const devoured = data.filter(b => b.devoured);
-
-        res.render('index', { uneaten, devoured });
-    }
-
     // Find all burgers in the DB
-    orm.selectAll(renderBurgers);
+    orm.selectAll(data => renderBurgers(res, data));
 });
 
-router.post('/:burger', (req, res) => {
+router.post('/burgers/:burger', (req, res) => {
     if (!req.params.burger) {
         res.status(422).send('Missing burger name.');
     }
 
-    orm.insertOne(req.params.burger);
+    orm.insertOne(req.params.burger, data => renderBurgers(res, data));
+});
+
+router.put('/burgers/:id', (req, res) => {
+    if (!req.params.id) {
+        res.status(422).send('Missing burger id.');
+    }
+
+    const burgerId = parseInt(req.params.id);
+
+    orm.updateOne(burgerId, data => renderBurgers(res, data));
+});
+
+router.delete('/burgers', (req, res) => {
+    orm.resetDb(data => renderBurgers(res, data));
 });
 
 module.exports = router;
